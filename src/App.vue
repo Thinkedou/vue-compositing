851cc83f5df9a3eee77b889bdf7f1834ef49afaa
Thinkedou <r.enguehard@gmail.com>
2022-03-26 17:31:11 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+      <div class='row'>
+            <div class='col left'>
+                <ul>
+                    <li>Ici c'est App.vue</li>
+                    <li>Parent (nav, button, etc)</li>
+                </ul>
+            </div>
+            <div class='col right'>
+              <CciCard />
+            </div>
+      </div>
+
+
+  </div>
+</template>
+
+<script>
+import CciCard from './components/CciCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CciCard
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
+.#app{
+	display: flex;
+	margin-left: 5%;
+	margin-top:2%;
+	margin-right:5%;
+
+}
+.row{
+	margin-top:10px;
+	width:100%;
+	display: flex;
+
+}
+.col {
+    padding:50px;
+    color:cyan;
+    font-family: 'Muli', sans-serif;
+}
+.left{
+    width:20%;
+    background-color: #065a75;
+}
+.right{
+    width:70%;
+}
+</style>
---
+++ src/components/CciCard.vue
@@ -0,0 +1,134 @@
+<template>
+    <div>
+        <div class="courses-container">
+            <div class="course">
+                <div class="course-preview">
+                        <h6>Course</h6>
+                        <h2>JavaScript Fundamentals</h2>
+                </div>
+                <div class="course-info">
+                    <h6>Chapter X</h6>
+                    <h2>Nom du chapitre</h2>
+                    <button class="btn">Continue</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'CciCard',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
+
+.courses-container {
+    font-family: 'Muli', sans-serif;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+    color:black;
+	flex-direction: column;
+}
+
+.course {
+	background-color: #fff;
+	border-radius: 10px;
+	box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
+	display: flex;
+	max-width: 100%;
+	margin: 20px;
+	overflow: hidden;
+	width: 700px;
+}
+
+.course h6 {
+	opacity: 0.6;
+	margin: 0;
+	letter-spacing: 1px;
+	text-transform: uppercase;
+}
+
+.course h2 {
+	letter-spacing: 1px;
+	margin: 10px 0;
+}
+
+.course-preview {
+	background-color: #2A265F;
+	color: #fff;
+	padding: 30px;
+	max-width: 250px;
+}
+
+.course-preview a {
+	color: #fff;
+	display: inline-block;
+	font-size: 12px;
+	opacity: 0.6;
+	margin-top: 30px;
+	text-decoration: none;
+}
+
+.course-info {
+	padding: 30px;
+	position: relative;
+	width: 100%;
+}
+
+.progress-container {
+	position: absolute;
+	top: 30px;
+	right: 30px;
+	text-align: right;
+	width: 150px;
+}
+
+.progress {
+	background-color: #ddd;
+	border-radius: 3px;
+	height: 5px;
+	width: 100%;
+}
+
+.progress::after {
+	border-radius: 3px;
+	background-color: #2A265F;
+	content: '';
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 5px;
+	width: 66%;
+}
+
+.progress-text {
+	font-size: 10px;
+	opacity: 0.6;
+	letter-spacing: 1px;
+}
+
+.btn {
+	background-color: #2A265F;
+	border: 0;
+	border-radius: 10px;
+	box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
+	color: #fff;
+	font-size: 16px;
+	padding: 12px 25px;
+	position: absolute;
+	bottom: 30px;
+	right: 30px;
+	letter-spacing: 1px;
+}
+
+</style>
